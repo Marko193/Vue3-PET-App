f906ac2f6611b3514ceb2c5bc7ac3ab7c7ea58d8
Marko193 <marko.pavlenko@nure.ua>
2023-10-09 00:56:02 +0400

    feat: init commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <post-form
+        @create="createPost"
+    />
+    <post-list
+        :posts="posts"
+
+    />
+  </div>
 </template>
 
+<script>
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+
+export default {
+  components: {
+    PostForm,
+    PostList
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание №1'},
+        {id: 2, title: 'C++', body: 'Описание №2'},
+        {id: 3, title: 'Python', body: 'Описание №3'}
+      ]
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      // console.log('post', post);
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/PostForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+        type="text"
+        class="input"
+        placeholder="Название"
+        v-model="post.title"
+    >
+
+    <input
+        type="text"
+        class="input"
+        placeholder="Описание"
+        v-model="post.body"
+    >
+
+    <button class="btn" @click="createPost">Create</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: '',
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post);
+      this.post = {
+        title: '',
+        body: '',
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 10px;
+}
+
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
+.btn:hover {
+  cursor: pointer;
+}
+</style>
